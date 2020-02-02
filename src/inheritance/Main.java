5041ae5207de1b1776fb9149bc6604c780bbb311
laurentziudmm <laurentziudmm@gmail.com>
2020-02-02 15:19:14 +0200

    update abstract

--- src/inheritance/Main.java
+++ src/inheritance/Main.java
@@ -14,13 +14,24 @@ public class Main {
         System.out.println(trainer.getName()+ " " + trainer.getRating());
         System.out.println(student.getName() + " " + student.getGrade());
 
-        person.walk();
-        trainer.walk();
-        student.walk();
+//        person.walk();
+//        trainer.walk();
+//        student.walk();
 
         System.out.println(person.toString());
         System.out.println(trainer.toString());
         System.out.println(student.toString());
+
+        Person p = new Student("Alex " , 78);
+        Person t = new Trainer("Marian",345.67F);
+        Student s = new Student("Andrei",34);
+        s.getGrade();
+        p.walk();
+
+        //Facem cast
+
+//        Student s2 = (Student) t;
+//        s2.getGrade();
 
     }
 }
