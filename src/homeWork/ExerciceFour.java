cf95e3e3143290f235247355381e198a4bb1b303
laurentziudmm <laurentziudmm@gmail.com>
2020-02-04 18:01:27 +0200

    Added HomeWork

---
+++ src/homeWork/ExerciceFour.java
@@ -0,0 +1,16 @@
+package homeWork;
+
+public class ExerciceFour {
+    /**
+     * 3.A swimmer can swim and breath.A walker can walk and breath.Pornind de la cele 2 propozitii proiecteaza folosind clase si interfete.a.
+     * Creaza interfata Swim ce contine metoda void swim()
+     * b.Creaza interfata Breath ce contine metoda void breath()
+     * c.Creaza interfata Walk ce contine metoda void walk()
+     * d.Creaza clasa Swimmer ce implementeaza interfetele Swim si Breath si implementeaza metodele.
+     * Implementarea trebuie sa afiseze un mesaj sugestiv la consola.
+     * e.Creaza clasa Walker ce implementeaza interfetele Walk si Breath si implementeaza metodele.
+     * Implementarea trebuie sa afiseze un mesaj sugestiv la consola.
+     * f.In metoda main() instantiaza un obiect de tipul Walker si un obiect de tipulSwimmer.
+     * Pentru fiecare obiect apeleaza metodele lui si urmareste ce se afiseaza la consola.
+     */
+}
