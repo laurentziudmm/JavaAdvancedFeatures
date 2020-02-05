771befa806c03c260febe6abd92be1155a4dbb2e
laurentziudmm <laurentziudmm@gmail.com>
2020-02-05 08:47:52 +0200

    HomeWork Exercice One Finished

---
+++ src/homeWork/exerciceOne/Main.java
@@ -0,0 +1,12 @@
+package homeWork.exerciceOne;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        User user = new User("laurentiu", "Marian", "Craciun");
+
+        user.generateDisplayName();
+
+    }
+}
---
+++ src/homeWork/exerciceOne/User.java
@@ -0,0 +1,44 @@
+package homeWork.exerciceOne;
+
+public class User {
+
+    private String userName;
+    private String lastName;
+    private String firstName;
+
+    public User(String userName, String lastName, String firstName) {
+        this.userName = userName;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void generateDisplayName() {
+
+        System.out.println("User Name: " + userName + " \nFull Name: " + firstName + " " + lastName);
+    }
+}
+
