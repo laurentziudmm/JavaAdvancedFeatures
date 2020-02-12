4d8b215605273fc7adf6b113fd014e9dc5b5369e
laurentziudmm <laurentziudmm@gmail.com>
2020-02-12 08:52:09 +0200

    Finished ExThree

--- src/homeWork/exerciceTwo/exThree/Main.java
+++ src/homeWork/exerciceTwo/exThree/Main.java
@@ -3,6 +3,8 @@ package homeWork.exerciceTwo.exThree;
 public class Main {
     public static void main(String[] args) {
 
+        //ExThree Solved
+
 
         Walker walk = new Walker();
         walk.walk();
